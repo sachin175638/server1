6c070734571377c50e5a956b0cb1eb52bac5e77f
sachin175638 <35227965+sachin175638@users.noreply.github.com>
2018-08-22 14:36:17 +0530

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+#apt-get install git -y
+if [ -d $HOME/server1 ]
+then
+	rm -rf $HOME/server1
+fi
+git clone https://github.com/sachin175638/server1.git
+cd server1
+unzip server.zip
+cd server
+chmod +x setup.sh
+sh setup.sh
+rm -rf $HOME/server1
